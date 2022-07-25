795f870d725491be2f8520759c8deadbf213acdc
zaru <zarutofu@gmail.com>
2022-07-25 20:58:14 +0900

    脆弱性のあるコントローラー例を追加

---
+++ app/controllers/embed/vulnerable_articles_controller.rb
@@ -0,0 +1,41 @@
+class Embed::VulnerableArticlesController < ApplicationController
+  # CSRF 脆弱性のあるコントローラー例
+  #
+  # InvalidAuthenticityToken が発生しないようにする = authenticity_token のチェックをしない
+  skip_forgery_protection
+
+  # GET /articles/new
+  def new
+    response.headers['X-Frame-Options'] = "ALLOW-FROM http://good.example.jp:9000"
+    @article = Article.new
+  end
+
+  # POST /articles or /articles.json
+  def create
+    # Request Origin のチェックを確認しないので、許可していない悪意のあるサイトから JS による POST が成功してしまう
+    # Access-Control-Allow-Origin で制御をしているので、JS 自体はレスポンスを受け取れないが、サーバ側の実行はされている状態
+    response.headers['Access-Control-Allow-Origin'] = "http://good.example.jp:9000"
+    @article = Article.new(article_params)
+
+    respond_to do |format|
+      if @article.save
+        format.html { redirect_to embed_thanks_url, notice: "Article was successfully created." }
+        format.json { render json: { result: 'Success!' } }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @article.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_article
+      @article = Article.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def article_params
+      params.require(:article).permit(:title)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   resources :articles
   namespace :embed do
-    resources :articles
+    resources :articles, only: [:new, :create]
+    resources :vulnerable_articles, only: [:new, :create]
     resource :thanks, only: [:show]
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
