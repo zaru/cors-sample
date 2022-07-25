10f4d00d3fdf5b25c95c71a07c21e20390928fb7
zaru <zarutofu@gmail.com>
2022-07-25 16:27:41 +0900

    埋め込み用の最低限コントローラーを追加

---
+++ app/controllers/embed/articles_controller.rb
@@ -0,0 +1,34 @@
+class Embed::ArticlesController < ApplicationController
+
+  # GET /articles/new
+  def new
+    @article = Article.new
+  end
+
+
+  # POST /articles or /articles.json
+  def create
+    @article = Article.new(article_params)
+
+    respond_to do |format|
+      if @article.save
+        format.html { redirect_to article_url(@article), notice: "Article was successfully created." }
+        format.json { render :show, status: :created, location: @article }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @article.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_article
+      @article = Article.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def article_params
+      params.require(:article).permit(:title)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   resources :articles
+  namespace :embed do
+    resources :articles
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
