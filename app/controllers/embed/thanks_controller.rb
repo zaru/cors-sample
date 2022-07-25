820ed771bbe345db0d57d5b4fd8071eedf0dece5
zaru <zarutofu@gmail.com>
2022-07-25 22:29:14 +0900

    POST 後のページに関してはリファラが iframe 設置元 URL 取得できないため、問答無用で設置可能にする

--- app/controllers/embed/thanks_controller.rb
+++ app/controllers/embed/thanks_controller.rb
@@ -1,5 +1,10 @@
 class Embed::ThanksController < ApplicationController
   def show
-    response.headers['X-Frame-Options'] = "ALLOW-FROM http://good.example.jp:9000"
+    # request.referrer は http://form.example.com:3000/ になる
+    # つまりサンクスページを特定の URL に対してだけ許可する処理を動的にすることは難しい
+    # ALLOW-FROM が使えていた時代の時はホワイトリスト URL の数だけ許可するようにすれば良かったが…
+    # response.headers['X-Frame-Options'] = "ALLOW-FROM http://good.example.jp:9000"
+    # 廃止されているため問答無用で nil にし、X-Frame-Options 自体を出力しないようにしている
+    response.headers['X-Frame-Options'] = nil
   end
 end
