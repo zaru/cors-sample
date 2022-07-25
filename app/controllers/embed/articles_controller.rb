0929f2addc035f22db59e30a02d614964a9da04e
zaru <zarutofu@gmail.com>
2022-07-25 18:44:05 +0900

    クロスドメインサイトの JS から POST を実行できるように Access-Control-Allow-Origin を設定

--- app/controllers/embed/articles_controller.rb
+++ app/controllers/embed/articles_controller.rb
@@ -8,15 +8,15 @@ class Embed::ArticlesController < ApplicationController
     @article = Article.new
   end
 
-
   # POST /articles or /articles.json
   def create
+    response.headers['Access-Control-Allow-Origin'] = "http://good.example.jp:9000"
     @article = Article.new(article_params)
 
     respond_to do |format|
       if @article.save
         format.html { redirect_to embed_thanks_url, notice: "Article was successfully created." }
-        format.json { render :show, status: :created, location: @article }
+        format.json { render json: { result: 'Success!' } }
       else
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @article.errors, status: :unprocessable_entity }
